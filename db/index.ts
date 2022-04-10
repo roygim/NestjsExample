eee3e6b2e1ebd5bfff62f5d6f1968fdd12b07ac8
Roei Grumet <roygim@gmail.com>
2022-04-10 18:13:09 +0300

    .

---
+++ db/index.ts
@@ -0,0 +1,5 @@
+const usersDB = [{ email: 'aaa@gmail.com', password: 'aaa'}]
+
+module.exports = {
+    usersDB: usersDB
+};
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,15 @@
+const { usersDB } = require('../../db');
+
 import { Injectable } from '@nestjs/common';
 import { UserDto } from './dto';
 
-const users: UserDto [] = [{ email: 'aaa@gmail.com', password: 'aaa'}]
+const users: UserDto [] = usersDB;
 
 @Injectable()
 export class UserService {
   async getAllUsers() {
-    const retVal = users.filter(item => delete item.password);
-    return retVal;
+    const retVal = usersDB.filter(item => delete item.password);
+    return usersDB;
   }
 
   async addUser(dto: UserDto) {
