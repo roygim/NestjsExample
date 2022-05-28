7e96cfb5cf19cb3564094d1a203c4b8e23f0e64d
roygim <roygim@gmail.com>
2022-05-28 21:33:46 +0300

    .

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
-const UsersDB = [{ id: '1', email: 'aaa@gmail.com', password: 'aaa'}]
+const UsersDB = [{ id: '1', email: 'aaa@gmail.com', password: 'aaa'}, { id: '2', email: 'bbb@gmail.com', password: 'bbb'}]
 const ProductsDB = [{ id: '1', name: 'product1', price: '100'}, { id: '1', name: 'product2', price: '200'}]
 const CartsDB = [{ id: '1', userid: '', porducts: [1]}]
 
